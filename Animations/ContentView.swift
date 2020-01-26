25f07658dbd08f6ef16821847dfd8e6d9f580876
Joshua Hubbard <jhubardsf@gmail.com>
2020-01-26 02:20:58 -0500

    Custom transition animation using roatationEffect

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,29 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("Hello SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
-
+    @State private var isShowingRed = false
+    
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count) { num in
-                Text(String(self.letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(self.enabled ? Color.blue : Color.red)
-                    .offset(self.dragAmount)
-                    .animation(Animation.default.delay(Double(num) / 20))
+        VStack {
+            Button("Tap Me") {
+                // do nothing
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+                
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { self.dragAmount = $0.translation }
-                .onEnded { _ in
-                    self.dragAmount = .zero
-                    self.enabled.toggle()
-                }
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
         )
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
     }
 }
 
