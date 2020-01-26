83f02b83159ab3f52aa98e5b111fd2a50bde7f79
Joshua Hubbard <jhubardsf@gmail.com>
2020-01-26 01:18:37 -0500

    Adding animation to binding

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,45 +10,26 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount: CGFloat = 1
-    
+
     var body: some View {
-        Button("Tap Me") {
-//            self.animationAmount += 1
+        print(animationAmount)
+
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            Spacer()
+
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
-        .padding(40)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(Color.red)
-                .scaleEffect(animationAmount)
-                .opacity(Double(2 - animationAmount))
-                .animation(
-                    Animation.easeOut(duration: 1)
-                        .repeatForever(autoreverses: false)
-                )
-        )
-        .onAppear {
-            self.animationAmount = 2
-        }
-        
-//        .scaleEffect(animationAmount)
-//        .overlay(
-//            Circle()
-//                .stroke(Color.red)
-//                .scaleEffect(animationAmount)
-//                .opacity(Double(2 - animationAmount))
-//        )
-//        .animation(
-//            Animation.easeInOut(duration: 1)
-//                .repeatCount(3, autoreverses: true)
-//        )
-//        .animation(.easeInOut(duration: 2))
-//        .animation(.default)
-//        .blur(radius: (animationAmount - 1) * 3)
-//        .animation(.interpolatingSpring(stiffness: 50, damping: 1))
-        
     }
 }
 
