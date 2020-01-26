d6224ac1fc7da5e1b281c571c017a52664547842
Joshua Hubbard <jhubardsf@gmail.com>
2020-01-26 01:20:25 -0500

    Setting up for 3D effect

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,27 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount: CGFloat = 1
-
     var body: some View {
-        print(animationAmount)
-
-        return VStack {
-            Stepper("Scale amount", value: $animationAmount.animation(
-                Animation.easeInOut(duration: 1)
-                    .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-            Spacer()
-
-            Button("Tap Me") {
-                self.animationAmount += 1
-            }
-            .padding(40)
-            .background(Color.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
+        Button("Tap Me") {
+            // do nothing
         }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
     }
 }
 
